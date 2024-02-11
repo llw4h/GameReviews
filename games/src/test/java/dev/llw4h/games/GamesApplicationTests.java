17aff6d04d10a6b912bc5453589ee7337da8d445
llw4h <30249831+llw4h@users.noreply.github.com>
2024-02-11 13:55:07 +0800

    added initial files

---
+++ games/src/test/java/dev/llw4h/games/GamesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.llw4h.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
