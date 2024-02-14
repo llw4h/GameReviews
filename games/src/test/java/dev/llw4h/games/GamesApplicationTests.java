08e4a0f0cc3cf1b13f45d056ce1f0a274f17c21d
llw4h <30249831+llw4h@users.noreply.github.com>
2024-02-14 10:33:08 +0800

    Update GamesApplicationTests.java

--- games/src/test/java/dev/llw4h/games/GamesApplicationTests.java
+++ games/src/test/java/dev/llw4h/games/GamesApplicationTests.java
@@ -1,13 +1,22 @@
 package dev.llw4h.games;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.bson.assertions.Assertions.assertNotNull;
 
 @SpringBootTest
 class GamesApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		// Basic assertion to verify that the application context is loaded successfully
+		assertNotNull(applicationContext);
 	}
 
 }
